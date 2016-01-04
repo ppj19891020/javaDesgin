b75f97ec7726bd2b7f5f0a02c2c9d4580eb7fb1c
pangpeijie <pangpeijie@2014-20150425GV>
2016-01-04 20:13:20 +0800

    [ADD]工厂设计模式例子

---
+++ javaDesgin/src/factory/ExportFileApi.java
@@ -0,0 +1,17 @@
+package factory;
+
+/**
+ * 导出文件
+ * @author pangpeijie
+ *
+ */
+public interface ExportFileApi {
+
+	/**
+	 * 导出数据
+	 * @param data
+	 * @return
+	 */
+	public boolean export(String data);
+	
+}
